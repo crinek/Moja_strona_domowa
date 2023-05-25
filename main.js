79573d731ce5b46d402536cfd9f9255394428d04
Torin <133996547+crinek@users.noreply.github.com>
2023-05-25 20:08:22 +0200

    Moja strona domowa

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log('hello');
+
+let content = document.querySelector('.head__title--js');
+
+console.log(content);
+console.log(content.innerHTML);
+
+content.innerHTML = 'Dziś jest czwartek!';
